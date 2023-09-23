9b6acb2951d18fda3f636c754d5b7b38142804d9
Aravinda Rathnamalala <aravinda.rathnamalala@gmail.com>
2023-09-23 23:49:36 +0800

    refac

--- lib/ui/gradient_container.dart
+++ lib/ui/gradient_container.dart
@@ -1,18 +1,23 @@
 import 'package:first_app/ui/styled_text.dart';
 import 'package:flutter/material.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: const [
           Colors.white,
           Color.fromARGB(255, 235, 195, 242),
           Color.fromARGB(255, 160, 74, 175)
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ], 
+        begin: startAlignment, 
+        end: endAlignment),
       ),
       child: const Center(child: StyledText()),
     );
