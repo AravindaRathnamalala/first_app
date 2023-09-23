0b60c2b02511d8770891e7d35ab9d680615a9fdf
Aravinda Rathnamalala <aravinda.rathnamalala@gmail.com>
2023-09-23 22:07:12 +0800

    adding stylings

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,34 @@
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text("Hellooo")));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 215, 163, 224),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Colors.white,
+              Color.fromARGB(255, 235, 195, 242),
+              Color.fromARGB(255, 160, 74, 175)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              "helloo rrrr",
+              style: TextStyle(
+                color: Color.fromARGB(255, 2, 29, 51),
+                fontSize: 25.0,
+              ),
+            ), 
+          ),
+        ),
+      ),
+    ),
+  );
 }
